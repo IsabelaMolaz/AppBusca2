35e551eb94de38773e24af847edd3e9c357d45ba
CathyHeathcliff <isabela.molaz@etec.sp.gov.br>
2020-06-17 19:30:43 +0000

    Adicionar Cors

---
+++ src/UsuariosPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './Header';
+
+function UsuariosPage() {
+    return <div>
+        <Header/>
+        <table>
+            {
+                usuarios.map(item => (
+                    <tr>
+                        <td>{item.idUsuario}</td>
+                        <td>{item.Nome}</td>
+                        <td>{item.Telefone}</td>
+                        <td>{item.Email}</td>
+                        <td>{item.Senha}</td>
+                    </tr>
+                ))
+            }
+        </table>
+    </div>
+}
+
+export default UsuariosPage;
