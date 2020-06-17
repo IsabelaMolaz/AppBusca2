fe16a600b5f59f01770d4d28325e017de88ad290
CathyHeathcliff <isabela.molaz@etec.sp.gov.br>
2020-06-17 19:31:32 +0000

    Teste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import UsuariosPage from './UsuariosPage';
 
-function App() {
+function App() { 
     return <UsuariosPage/>
 }
 
--- src/UsuariosPage.js
+++ src/UsuariosPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 
-function UsuariosPage() {
+function UsuariosPage() { 
     return <div>
         <Header/>
         <table>
